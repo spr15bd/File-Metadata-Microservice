b19ba38350190ebef4cacf65c78197aaef9794d3
spr15bd <bevandady@gmail.com>
2019-07-12 23:51:05 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ var multer  = require('multer');
 var upload = multer();
 var app = express();
 
+
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +21,10 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', function(req, res){
+  res.json({message: "Test file analyse"});
 });
 
 app.listen(process.env.PORT || 3000, function () {
