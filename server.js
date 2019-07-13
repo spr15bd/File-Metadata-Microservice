81c9bf7e1445509a1d0618ab2db387d0f6633d4e
spr15bd <bevandady@gmail.com>
2019-07-13 23:48:09 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,26 +7,34 @@ var cors = require('cors');
 var multer  = require('multer');
 var upload = multer();
 var app = express();
-
+var port = process.env.PORT || 3000;
 
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-
-
+// Set storage engine
+const multerConf = {
+  storage: multer.diskStorage({
+    destination: function(req, file, next) {
+      next(null, './public');
+    },
+    filename: function(req, file, next) {
+      console.log(file);
+      next();
+    }
+  })
+}
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+
+app.post('/api/fileanalyse', multer(multerConf).single('upfile'), function(req, res){
+  //res.json({message: "Test file analyse"});
+  res.send("test post route upload");
 });
 
-app.post('/api/fileanalyse', function(req, res){
-  res.json({message: "Test file analyse"});
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.listen(port, function () {
+  console.log(`Node.js listening on port ${port}`);
 });
