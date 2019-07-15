c0fb21de4fe4b9af676ec3ffab0593cad7db989f
spr15bd <bevandady@gmail.com>
2019-07-15 22:44:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 var path = require('path');
 // require and use "multer"...
 var multer  = require('multer');
-const upload = multer();
+
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -14,28 +14,36 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 // Set storage engine
-const multerConf = {
-  storage: multer.diskStorage({
-    destination: function(req, file, next) {
-      next(null, './public');
-    },
+const multerConf = multer.diskStorage({
+    destination: './public/uploads/',
     filename: function(req, file, callback) {
-      console.log(file);
+      //console.log(file);
       callback(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
     }
-  })
-}
+})
+
 
 // Initialise upload
+const upload = multer({
+  storage: multerConf
+}).single('upfile');  
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
 
-app.post('/api/fileanalyse', multer(multerConf).single('upfile'), function(req, res){
+app.post('/api/fileanalyse', function(req, res){
   //res.json({message: "Test file analyse"});
-  res.send("test post route upload");
+  upload(req, res, function(error) {
+    if (error) {
+      res.send("Error");
+    } else {
+      console.log(req.file);
+      res.json({filename: req.file.filename, size: req.file.size});
+    }
+  });
+  //res.send("success");
 });
 
 app.listen(port, function () {
